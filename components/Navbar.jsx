39ccbb6818da202c2c839177ef10009c5330ed47
BSCFTMDEV <christianmarquez237@gmail.com>
2022-09-10 17:02:14 -0300

    u

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,44 +1,54 @@
-/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-unused-vars */
+/* eslint-disable eol-last */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable key-spacing */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable quotes */
+/* eslint-disable semi */
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link'
-import React, { useState } from 'react'
-import NavItem from './NavItem'
+/* eslint-disable jsx-a11y/alt-text */
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import NavItem from "./Navitem";
 
 const MENU_LIST = [
-  { text: 'Market', href: '/' },
-  { text: 'Mint', href: '/Mint' },
-  { text: 'Mining', href: '/Mining' },
-  { text: "My Nft's", href: '/portal' },
-  { text: 'Farms', href: 'http://Dogebox.netlify.app' }
-]
+  { text: "Market", href: "/" },
+  { text: "Mint", href: "/Mint" },
+  { text: "Mining", href: "/Mining" },
+  { text: "My Nft's", href: "/portal" },
+  { text: "Farms", href: "http://Dogebox.netlify.app" },
+];
 const Navbar = () => {
-  const [navActive, setNavActive] = useState(null)
-  const [activeIdx, setActiveIdx] = useState(-1)
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
 
   return (
     <header>
-      <nav className={'nav'}>
-        <Link href={'/'}>
+      <nav className={`nav`}>
+        <Link href={"/"}>
           <a>
-             <img style={{ width: '140px' }} src ="Logo.png"/>
-            <h1 className="logo" style={{ color: '#FFFFFF' }}></h1>
 
+             <img style={{width:'140px'}} src ="Logo.png"/>
+            <h1 className="logo" style={{color:"#FFFFFF"}}></h1>
+            
           </a>
         </Link>
         <div
           onClick={() => setNavActive(!navActive)}
-          className={'nav__menu-bar'}
+          className={`nav__menu-bar`} 
         >
           <div></div>
           <div></div>
           <div></div>
         </div>
-        <div className={`${navActive ? 'active' : ''} nav__menu-list`}>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
           {MENU_LIST.map((menu, idx) => (
             <div
               onClick={() => {
-                setActiveIdx(idx)
-                setNavActive(false)
+                setActiveIdx(idx);
+                setNavActive(false);
               }}
               key={menu.text}
             >
@@ -48,7 +58,7 @@ const Navbar = () => {
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
