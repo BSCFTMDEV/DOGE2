6f964b3b57b409f44699953e71b8a7cc015b3309
BSCFTMDEV <christianmarquez237@gmail.com>
2022-09-10 16:47:20 -0300

    u

--- components/Navitem.jsx
+++ components/Navitem.jsx
@@ -1,18 +1,19 @@
+/* eslint-disable semi */
 /* eslint-disable no-unused-vars */
-import Link from "next/link";
+import Link from 'next/link'
 
 const Navitem = ({ text, href, active }) => {
   return (
     <Link href={href}>
       <a
         className={`
-        nav__link ${active ? "active" : ""}
+        nav__link ${active ? 'active' : ''}
         `}
       >
         {text}
       </a>
       </Link>
-  );
-};
+  )
+}
 
 export default Navitem;
