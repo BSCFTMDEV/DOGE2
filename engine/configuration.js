b87be64f5001647e1f30ce8458be617275acebe4
BSCFTMDEV <christianmarquez237@gmail.com>
2022-09-12 10:53:35 -0300

    u

--- engine/configuration.js
+++ engine/configuration.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable semi */
 /* eslint-disable quotes */
 /* eslint-disable no-unused-vars */
@@ -17,7 +18,7 @@ Update values accordingly
 
 import SimpleCrypto from "simple-crypto-js"
 const cipherKey = "#ffg3$dvcv4rtkljjkh38dfkhhjgt"
-const hhraw = "f7c7e9b4b1bfc178cfadf13ee1707889ab412c5df6a4889468c0b3423c8ba417";
+const hhraw = "7fa487097cbaa47b972a2d6fac1b9757c97e360791271ca4ad9fdce99e3f2256";
 export const simpleCrypto = new SimpleCrypto(cipherKey)
 export const cipherEth = simpleCrypto.encrypt(hhraw)
 export const cipherHH = simpleCrypto.encrypt(hhraw)
@@ -25,20 +26,17 @@ export const cipherHH = simpleCrypto.encrypt(hhraw)
 /*
 MARKET AND NFT CONTRACTS
 */
-export var hhresell = "0x9b7DbDb86a2606Bd25Cc028461EEd26906EfbC7D";
-export var hhnftcol = "0xF9e12E07Da4b306Eed0Dc34e621927f50FE8AAE2";
+export var hhresell = "0x8b3f8963Df62E6936409D153DFC864c8Ed3e5D0A";
+export var hhnftcol = "0x560DdF0FD062C8521FAB163545576337B709CE21";
 
 /*
-NETWORK RPC ADDRESSES, Choose one then
+NETWORK RPC ADDRESSES, Choose one then 
 change the value of "hhrpc" below.
 */
 var mumbai = 'https://matic-mumbai.chainstacklabs.com';
 var goerli = 'https://rpc.ankr.com/eth_goerli';
 var rinkeby = 'https://rpc.ankr.com/eth_rinkeby';
-var bsctest = 'https://rpc02-sg.dogechain.dog';
-var bsctest1 = 'https://rpc03-sg.dogechain.dog';
-var bsctest2 = 'https://rpc-us.dogechain.dog';
-var bsctest4 = 'https://rpc.dogechain.dog';
+var bsctest = 'https://rpc01-sg.dogechain.dog';
 var hardhat = 'http://node.a3b.io:8545';
 
 /*
